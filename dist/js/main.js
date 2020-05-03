52ed0fa06389a0dc6c615bb3f187a8c29a4dedb0
Taysiabee <50152275+Taysiabee@users.noreply.github.com>
2020-05-03 10:04:01 -0400

    sixties audio

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,40 @@
 "use strict";
 
+// 5 to 7 tv images
+// 5 to 7 audios
+// User story:
+// when the user hovers over a TV, the matching audio plays
+// when the user moves their mouse off the TV, the audio pauses
+// 
+// Developer story:
+// find ALL the TVs
+// find ALL the audios
+// loop through all the TVs
+//     when the mouseenter event happens to TV with the number from the loop counter
+//     tell audio with the number from the loop counter to play
+// 
+// Code Story:
+// querySelectorAll
+// querySelectorAll
+// loop
+// [] to get stuff from an array
+// addEventlistener
+// .play()
 document.querySelector('.play').addEventListener('click', function () {
-  var audio = document.querySelector("audio");
+  var audio = document.querySelector('.chubby');
   audio.play();
 });
 document.querySelector('.pause').addEventListener('click', function () {
-  var audio = document.querySelector("audio");
+  var audio = document.querySelector('.chubby');
   audio.pause();
 });
 var sixtiesVideo = document.querySelector('.sixties-video');
-var audio = document.querySelector("audio");
+var audio = document.querySelector('.chubby');
 sixtiesVideo.addEventListener("mouseenter", function (event) {
   audio.play();
 });
 sixtiesVideo.addEventListener("mouseleave", function (event) {
-  var audio = document.querySelector("audio");
+  var audio = document.querySelector('.chubby');
   audio.pause();
-}); // document.querySelector('.playing').addEventListener('click', function() {
-// 	let audio2 = document.querySelector("audio-two");
-// 	audio2.play();
-// 	console.log('sdf')
-// })
-// document.querySelector('.paused').addEventListener('click', function() {
-// 	let audio = document.querySelector("audio");
-// 	audio.pause()
-// })
+});
 //# sourceMappingURL=main.js.map
